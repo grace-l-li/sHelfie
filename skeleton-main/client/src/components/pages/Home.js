a441e2a91b23b5fb46f2a6ffa7c4e5d44ac209dd
Franklin Nguyen <contactfnguyen@gmail.com>
2024-01-22 19:52:09 -0500

    feed

--- skeleton-main/client/src/components/pages/Home.js
+++ skeleton-main/client/src/components/pages/Home.js
@@ -1,22 +1,31 @@
 import React, { useState, useEffect } from "react";
-import "../../utilities.css";
-import "./Profile.css";
 import NavBar from "../modules/NavBar.js";
-import Feed from "../modules/Feed.js"; // import the Feed component
+import Feed from "../modules/Feed.js";
+import "../../utilities.css";
+import "./Home.css";
 
 const Home = () => {
   useEffect(() => {
-    document.title = "Home";
+    // Set the background color when the component mounts
+    document.body.style.backgroundColor = "var(--grey)"; // Assuming var(--grey) is defined in your CSS
+
+    // Cleanup function to reset the background color when the component unmounts
+    return () => {
+      document.body.style.backgroundColor = ""; // Reset to default or previous color
+    };
   }, []);
 
   return (
     <div>
       <NavBar />
-      <Feed />
-      {/* <div className="Home-container">
+      <div className="Home-container">
         <Feed className="Feed-flex" />
-        <div className="Friends-flex">hi</div>
-      </div> */}
+        <div className="Friends-flex">
+          <h3 className="Friends-header">Friend Requests</h3>
+
+          <h3 className="Friends-header">Friends</h3>
+        </div>
+      </div>
     </div>
   );
 };
